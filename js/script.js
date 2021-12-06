6dd28bb63e7154c3d02a1d50e64d0da26432ed9c
borang <lbr@uaram.co.kr>
2021-12-06 17:57:47 +0900

    21.12.06

--- js/script.js
+++ js/script.js
@@ -22,17 +22,6 @@
       $(this).find(".accordian-arrow").toggleClass("less");
     });
 
-    $(".btn").click(function() {
-      $("#menu,.page_cover,html").addClass("open");
-      window.location.hash = "#open";
-    });
-    
-    window.onhashchange = function() {
-      if (location.hash != "#open") {
-        $("#menu,.page_cover,html").removeClass("open");
-      }
-    };
-    
   });
   
 
