a12c8144f41f8e8c4602b801c23e3c0308745f79
borang <lbr@uaram.co.kr>
2021-12-10 08:50:41 +0900

    21.12.10

--- js/script.js
+++ js/script.js
@@ -5,15 +5,12 @@
         auto:true, mode:'fade', prevText:'', nextText:'', speed:500, 
     });
 
-  $('.tab_menu_btn').on('click',function(){
-
-    $('.tab_menu_btn').removeClass('on');
-    $(this).addClass('on')
-    
-    var idx = $('.tab_menu_btn').index(this);
-    
-    $('.tab_box').hide();
-    $('.tab_box').eq(idx).show();
+    $('.tab_menu_btn').on('click',function(){
+      $('.tab_menu_btn').removeClass('on');
+      $(this).addClass('on')
+      var idx = $('.tab_menu_btn').index(this);
+      $('.tab_box').hide();
+      $('.tab_box').eq(idx).show();
   });
 
   $(document).ready(function(){
@@ -21,19 +18,39 @@
       $(this).nextAll(".content:first").slideToggle("fast");
       $(this).find(".accordian-arrow").toggleClass("less");
     });
+
+  $(document).ready(function(){
+    $(".info--accordian1").click(function() {
+      $(this).nextAll(".content1:first").slideToggle("fast");
+      $(this).find(".accordian-arrow").toggleClass("less");
+    });
+
+    $(document).ready(function(){
+      $(".info--accordian2").click(function() {
+        $(this).nextAll(".content2:first").slideToggle("fast");
+        $(this).find(".accordian-arrow").toggleClass("less");
+      });
+
+    $(document).ready(function(){
+      $(".info--accordian3").click(function() {
+        $(this).nextAll(".content3:first").slideToggle("fast");
+        $(this).find(".accordian-arrow").toggleClass("less");
+      });
+    });
+  });
+});
     
     $(".btn").click(function() {
-      $("#menu,.page_cover,html").addClass("open");
+      $(".side__menu,.page_cover,html").addClass("open");
       window.location.hash = "#open";
     });
     
     window.onhashchange = function() {
       if (location.hash != "#open") {
-        $("#menu,.page_cover,html").removeClass("open");
+        $(".side__menu,.page_cover,html").removeClass("open");
       }
     };
   });
-  
 
   $(document).ready(function() {
     $('.minus').click(function () {
@@ -63,3 +80,18 @@
       checkbox.checked = selectAll.checked;
     })
   }  
+
+  var modal = document.getElementById('myModal');
+  var btn = document.getElementById("myBtn");
+  var span = document.getElementsByClassName("close")[0];
+  btn.onclick = function() {
+      modal.style.display = "block";
+  }
+  span.onclick = function() {
+      modal.style.display = "none";
+  }
+  window.onclick = function(event) {
+      if (event.target == modal) {
+          modal.style.display = "none";
+      }
+  } 
