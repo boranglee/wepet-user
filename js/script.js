7289308c926555a55daa15edcdff33bde047b047
borang <lbr@uaram.co.kr>
2021-12-24 15:37:16 +0900

    21.12.24

--- js/script.js
+++ js/script.js
@@ -73,21 +73,4 @@
     checkboxes.forEach((checkbox) => {
       checkbox.checked = selectAll.checked;
     })
-  }  
-
-  var modal = document.getElementById('myModal');
-  var btn = document.getElementById("myBtn");
-  var span = document.getElementsByClassName("close")[0];
-  btn.onclick = function() {
-      modal.style.display = "block";
-  }
-  span.onclick = function() {
-      modal.style.display = "none";
-  }
-  window.onclick = function(event) {
-      if (event.target == modal) {
-          modal.style.display = "none";
-      }
-  } 
-
- 
+  }   
